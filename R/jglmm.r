dafda67b28afc37ed8f991cf711035111cff362e
Mika Braginsky <mika.br@gmail.com>
2021-01-27 16:34:30 -0500

    add ranef extraction function

--- R/jglmm.r
+++ R/jglmm.r
@@ -148,3 +148,29 @@ augment.jglmm <- function(x) {
   x$data$.fitted <- fits
   x$data %>% dplyr::as_tibble()
 }
+
+#' Extract the modes of the random effects
+#'
+#' Extract the conditional modes of the random effects from a fitted `jglmm`
+#' object.
+#'
+#' @param x An object of class `jglmm`, as returned by `jglmm`.
+#'
+#' @return A list of tibbles, one for each random effect group.
+#' @export
+#'
+#' @examples
+#' \dontrun{
+#' cbpp <- dplyr::mutate(lme4::cbpp, prop = incidence / size)
+#' gm <- jglmm(prop ~ period + (1 | herd), data = cbpp, family = "binomial",
+#'             weights = cbpp$size)
+#' ranef(gm)
+#' }
+ranef_jglmm <- function(x) {
+  julia_assign("model", x$model)
+  julia_command("model_ranef = map(DataFrame, raneftables(model));")
+  model_ranef <- julia_eval("model_ranef")
+  ranef_terms <- julia_eval("keys(model_ranef)")
+  purrr::map(1:length(model_ranef), ~dplyr::as_tibble(model_ranef[.x])) %>%
+    purrr::set_names(ranef_terms)
+}
