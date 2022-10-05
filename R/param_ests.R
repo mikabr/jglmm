f23a6045c666a38b369fc937eeda228d65872e59
Mika Braginsky <mika.br@gmail.com>
2022-10-04 17:28:01 -0700

    add methods for fixef, vcov, sigma

--- R/param_ests.R
+++ R/param_ests.R
@@ -1,16 +1,74 @@
-#' Title
+#' @keywords internal
+coef_trans <- function(coef_names) {
+  coef_names |> stringr::str_remove(": ")
+}
+
+#' Extract fixed-effects estimates
 #'
-#' @param x
+#' @importFrom lme4 fixef
+#' @param x An object of class `jglmm`, as returned by `jglmm()`.
+#'
+#' @return A named numeric vector of fixed-effects estimates.
+#' @export
+#'
+#' @examples
+#' \dontrun{
+#' jglmm_setup()
+#' lm1 <- jglmm(Reaction ~ Days + (Days | Subject), lme4::sleepstudy)
+#' fixef(lm1)
+#' }
+fixef.jglmm <- function(x) {
+  julia_assign("model", x$model)
+  fixef_vals <- julia_eval("fixef(model);")
+  fixef_names <- julia_eval("fixefnames(model);") |> coef_trans()
+  rlang::set_names(fixef_vals, fixef_names)
+}
+
+#' Calculate variance-covariance matrix for a fitted model object
+#'
+#' @importFrom stats vcov
+#' @param x An object of class `jglmm`, as returned by `jglmm()`.
+#'
+#' @return A matrix of the estimated covariances between the parameter estimates
+#'   in the linear or non-linear predictor of the model.
+#' @export
+#'
+#' @examples
+#' \dontrun{
+#' jglmm_setup()
+#' lm1 <- jglmm(Reaction ~ Days + (Days | Subject), lme4::sleepstudy)
+#' vcov(lm1)
+#' }
+vcov.jglmm <- function(x) {
+  julia_assign("model", x$model)
+  vcov_matrix <- julia_eval("vcov(model);")
+  coef_names <- julia_eval("coefnames(model);") |> coef_trans()
+  colnames(vcov_matrix) <- coef_names
+  rownames(vcov_matrix) <- coef_names
+  return(vcov_matrix)
+}
+
+#' Extract residual standard deviation 'sigma'
+#'
+#' Extract the estimated standard deviation of the errors, the "residual
+#' standard deviation" (also misnamed the "residual standard error"), from a
+#' fitted model.
+#'
+#' @importFrom stats sigma
+#' @param x An object of class `jglmm`, as returned by `jglmm()`.
 #'
 #' @return
 #' @export
 #'
 #' @examples
-fixef.jglmm <- function(x) {
+#' \dontrun{
+#' jglmm_setup()
+#' lm1 <- jglmm(Reaction ~ Days + (Days | Subject), lme4::sleepstudy)
+#' sigma(lm1)
+#' }
+sigma.jglmm <- function(x) {
   julia_assign("model", x$model)
-  vals <- julia_eval("fixef(model);")
-  names <- julia_eval("fixefnames(model);")
-  rlang::set_names(vals, names)
+  julia_eval("sdest(model);")
 }
 
 #' Extract the modes of the random effects
@@ -18,7 +76,7 @@ fixef.jglmm <- function(x) {
 #' Extract the conditional modes of the random effects from a fitted `jglmm`
 #' object.
 #'
-#' @param x An object of class `jglmm`, as returned by `jglmm`.
+#' @param x An object of class `jglmm`, as returned by `jglmm()`.
 #'
 #' @return A list of tibbles, one for each random effect group.
 #' @export
