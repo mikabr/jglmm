37ee89485eca7bb180a2278860faf824d295f0af
Mika Braginsky <mika.br@gmail.com>
2022-10-08 13:26:58 -0700

    add doc stubs for datasets

---
+++ R/data.R
@@ -0,0 +1,68 @@
+#' cbpp
+#'
+#' @format A tibble
+"cbpp"
+
+#' contra
+#' @format A tibble
+"contra"
+
+#' d3
+#' @format A tibble
+"d3"
+
+#' dyestuff
+#' @format A tibble
+"dyestuff"
+
+#' dyestuff2
+#' @format A tibble
+"dyestuff2"
+
+#' grouseticks
+#' @format A tibble
+"grouseticks"
+
+#' insteval
+#' @format A tibble
+"insteval"
+
+#' kb07
+#' @format A tibble
+"kb07"
+
+#' machines
+#' @format A tibble
+"machines"
+
+#' ml1m
+#' @format A tibble
+"ml1m"
+
+#' mmec
+#' @format A tibble
+"mmec"
+
+#' mrk17_exp1
+#' @format A tibble
+"mrk17_exp1"
+
+#' oxide
+#' @format A tibble
+"oxide"
+
+#' pastes
+#' @format A tibble
+"pastes"
+
+#' penicillin
+#' @format A tibble
+"penicillin"
+
+#' sleepstudy
+#' @format A tibble
+"sleepstudy"
+
+#' verbagg
+#' @format A tibble
+"verbagg"
